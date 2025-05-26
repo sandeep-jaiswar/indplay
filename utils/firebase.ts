90d18f85ceab09f034340b622ff0797d2ecf0afb
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2025-05-26 16:15:35 +0530

    feat: add Firebase initialization and Firestore collections setup

---
+++ utils/firebase.ts
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app"
+
+import { collection, getFirestore } from "firebase/firestore"
+import { getStorage } from "firebase/storage"
+
+const clientCredentials = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+}
+
+const app = initializeApp(clientCredentials)
+
+const db = getFirestore(app)
+const storage = getStorage(app)
+
+const postsCollection = collection(db, "posts")
+const commentsCollection = collection(db, "comments")
+const usersCollection = collection(db, "users")
+
+export { app, db, storage, postsCollection, commentsCollection, usersCollection }
